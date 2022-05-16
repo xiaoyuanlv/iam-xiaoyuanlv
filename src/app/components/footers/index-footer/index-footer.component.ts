648e85fca78d138834729f93dcb1a063d6e45265
xingyonglv <xingyonglv@gmail.com>
2022-05-17 05:20:25 +0630

    Simple readme

--- src/app/components/footers/index-footer/index-footer.component.ts
+++ src/app/components/footers/index-footer/index-footer.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class IndexFooterComponent implements OnInit {
 
+  currentYear = new Date().getFullYear();
+
   constructor() {
    
    }
