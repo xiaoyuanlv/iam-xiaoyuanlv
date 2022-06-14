34dbc4bb00342f82d53926aa51065b431279500b
xingyonglv <xingyonglv@gmail.com>
2022-06-14 18:59:20 +0630

    Terms Dynamic

--- src/app/pages/apps/apps-terms/apps-terms.component.ts
+++ src/app/pages/apps/apps-terms/apps-terms.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+import { Appgallery } from 'src/app/models/appgallery.model';
 import { Mapp } from 'src/app/models/mapp.model';
+import { AppgalleryService } from 'src/app/shared/services/appgallery.service';
 
 @Component({
   selector: 'app-apps-terms',
@@ -9,25 +11,30 @@ import { Mapp } from 'src/app/models/mapp.model';
 })
 export class AppsTermsComponent implements OnInit {
 
-  id: any = '';
-  appname: String = '';
-  appArr: Array<Mapp> = [];
+  
+  id: any = 0;
+  appArr: Array<Appgallery> = [];
 
-  constructor(private route: ActivatedRoute, private router: Router) {
-    this.appArr[0] = new Mapp("1", "Dhammapada") 
-    this.appArr[1] =  new Mapp("2", "Planet") 
+  constructor(private route: ActivatedRoute, private appGallerySv: AppgalleryService) {
+    this.id = this.route.snapshot.paramMap.get('id');
   }
 
   ngOnInit(): void {
-    this.id = this.route.snapshot.paramMap.get('id');
-    const theapp = this.appArr.filter(a => {
-      return a.id === this.id;
-    });
-    if(theapp.length > 0) {
-      this.appname = theapp[0].title!!.toString();
-    } else {
-      this.router.navigate(['/']);
+    this.getList()
+  }
+
+  getList() {
+    if (this.id != 0) {
+      this.appGallerySv.getAppGallerybyID(Number(this.id)).subscribe(data => {
+        this.appArr = data.map(e => {
+          return {
+            id: e.payload.doc.id,
+            ...e.payload.doc.data() as Object
+          } as Appgallery;
+        })
+      });
     }
   }
+
 
 }
